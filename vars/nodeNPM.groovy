e7769dbcacafaccf1c9550193828d45b124a192c
siva <siva@ivs.tv>
2023-03-30 07:53:18 +0530

    msg

--- vars/nodeNPM.groovy
+++ vars/nodeNPM.groovy
@@ -55,13 +55,13 @@ def call(Map configMap){
         stage('Image push to ECR'){
             steps{
                 script{
-                withAWS(credentials: 'aws-auth', region: "${REGION}") {
-                    sh """
-                        aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
-                    docker tag ${IMAGE_REPO}:latest  ${REPO_URI}:latest
-                    docker push ${REPO_URI}:latest
-                    """
-                }
+                    withAWS(credentials: 'aws-auth', region: "${REGION}") {
+                        sh """
+                            aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
+                        docker tag ${IMAGE_REPO}:latest  ${REPO_URI}:latest
+                        docker push ${REPO_URI}:latest
+                        """
+                    }
                 }
             }
         }
