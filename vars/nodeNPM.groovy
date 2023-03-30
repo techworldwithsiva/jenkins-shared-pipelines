6440dfce92aa422cd375cd06a8acae97a96db05f
siva <siva@ivs.tv>
2023-03-30 07:52:27 +0530

    msg

--- vars/nodeNPM.groovy
+++ vars/nodeNPM.groovy
@@ -6,11 +6,11 @@ def call(Map configMap){
     pipeline{
         agent any
         environment{
-            AWS_ACCOUNT_ID="084767242532"
-            REGION="ap-southeast-1"
-            IMAGE_REPO="student-ui"
+            AWS_ACCOUNT_ID="752692907119"
+            REGION="ap-south-1"
+            IMAGE_REPO="hello-world-web"
             IMAGE_TAG="latest"
-            REPO_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${IMAGE_REPO}"
+            REPO_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${IMAGE_REPO}:${IMAGE_TAG}"
             CI=false
         }
         /* options{
@@ -46,14 +46,20 @@ def call(Map configMap){
                 }
             }
         }
+        withAWS(credentials: 'My AWS Credentials', region: "${env.AWS_REGION}") {
+                    sh """
+                        aws eks update-kubeconfig --name my-eks-cluster
+                        kubectl apply -f my-deployment.yaml
+                    """
+                }
         stage('Image push to ECR'){
             steps{
-                script{
-                    sh '''
-                    aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
+                withAWS(credentials: 'aws-auth', region: "${REGION}") {
+                    sh """
+                        aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
                     docker tag ${IMAGE_REPO}:latest  ${REPO_URI}:latest
                     docker push ${REPO_URI}:latest
-                    '''
+                    """
                 }
             }
         }
